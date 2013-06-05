30c4e2e0aff9f7faf85106aea8abcec73f0651bc
wear <wear63659220@gmail.com>
2013-06-05 16:10:12 +0800

    initial

---
+++ files/app/app_delegate.rb
@@ -0,0 +1,14 @@
+class AppDelegate
+  def application(application, didFinishLaunchingWithOptions:launchOptions)
+    true
+  end
+
+  def setWindow(new_window)
+    @window = new_window
+  end
+
+  def window
+    @window
+  end
+
+end
